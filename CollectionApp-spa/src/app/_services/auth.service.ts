9b7b9d76e81e0ca51b4c9343ec75cf1571a2bcb5
kaixiang <kaixiang.ren00@gmail.com>
2020-06-02 19:14:25 +1000

    Added Support
    - [x] Added Message Alert Support
    
    - [x] Added token management
    
    - [x] Added animation support
    
    - [x] Added theme

--- CollectionApp-spa/src/app/_services/auth.service.ts
+++ CollectionApp-spa/src/app/_services/auth.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/Operators';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   baseUrl = 'http://localhost:5000/api/auth/';
+  jwtHelper = new JwtHelperService();
+  decodedToken: any;
 
   constructor(private http: HttpClient) {}
 
@@ -19,6 +22,11 @@ export class AuthService {
         }
       })
     );
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !this.jwtHelper.isTokenExpired(token);
   }
 
   register(model: any) {
